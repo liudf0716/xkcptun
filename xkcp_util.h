6bbe170e286dcce25a7a20124ab64af8e5e6d7dd
Dengfeng Liu <liudf0716@qq.com>
2017-03-31 15:18:54 +0800

    Update xkcp_util.h

--- xkcp_util.h
+++ xkcp_util.h
@@ -5,6 +5,32 @@
 
 #define HTTP_IP_ADDR_LEN	16
 
+struct eventbase;
+struct sockaddr_in;
+struct bufferevent;
+
+struct xkcp_proxy_param {
+	struct event_base *base;
+	int 	udp_fd;
+	struct sockaddr_in	serveraddr;
+};
+
+struct xkcp_event_param {
+	struct event_base 	*base;
+	void	*args;
+};
+
+typedef	struct xkcp_event_param	xkcp_event_param_type;
+
+struct xkcp_task {
+	iqueue_head			head;
+	ikcpcb				*kcp;
+	struct bufferevent 	*b_in;
+	struct sockaddr_in	*svr_addr;
+};
+
+typedef struct xkcp_task xkcp_task_type;
+
 void itimeofday(long *sec, long *usec);
 
 IINT64 iclock64(void);
