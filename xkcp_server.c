08c63a049e5d54aa11b60bc72eba290d98011d0a
Dengfeng Liu <liudf0716@qq.com>
2017-04-07 20:36:41 +0800

    Update xkcp_server.c

--- xkcp_server.c
+++ xkcp_server.c
@@ -29,6 +29,16 @@
 
 IQUEUE_HEAD(xkcp_task_list);
 
+static int
+xkcp_output(const char *buf, int len, ikcpcb *kcp, void *user)
+{
+	struct xkcp_proxy_param *ptr = user;
+	int nret = sendto(ptr->udp_fd, buf, len, 0, &ptr->serveraddr, sizeof(ptr->serveraddr));
+	debug(LOG_DEBUG, "xkcp output [%d] [%d], send datagram from %d (%s)", 
+		  ptr->udp_fd, len, nret, strerror(errno));
+	return nret;
+}
+
 static void timer_event_cb(int nothing, short int which, void *ev)
 {
 	struct event *timeout = ev;
@@ -41,6 +51,46 @@ static void timer_event_cb(int nothing, short int which, void *ev)
 	}
 	
 	evtimer_add(timeout, &TIMER_TV);
+}
+
+static void xkcp_rcv_cb(const int sock, short int which, void *arg)
+{	
+	char buf[BUF_RECV_LEN] = {0};
+
+	/* Recv the data, store the address of the sender in server_sin */
+	int len = recvfrom(sock, &buf, sizeof(buf) - 1, 0, (struct sockaddr *) &param->serveraddr, &param->addr_len);
+	if (len > 0) {
+		int conv = ikcp_getconv(buf);
+		ikcpcb *kcp_client = get_kcp_from_conv(conv, &xkcp_task_list);
+		debug(LOG_DEBUG, "conv is %d, kcp_client is %d", conv, kcp_client?1:0);
+		if (kcp_client == NULL) {
+			struct xkcp_proxy_param *param = malloc(sizeof(struct xkcp_proxy_param));
+			memset(param, 0, sizeof(struct xkcp_proxy_param));
+			param->udp_fd = sock;
+			param->addr_len = sizeof(struct sockaddr_in);
+			kcp_client = ikcp_create(conv, param);
+			kcp_client->output	= xkcp_output;
+			ikcp_wndsize(kcp_client, 128, 128);
+			ikcp_nodelay(kcp_client, 0, 10, 0, 1);
+		}
+		
+		ikcp_input(kcp_client, buf, len);
+
+		struct xkcp_task *task = malloc(sizeof(struct xkcp_task));
+		assert(task);
+		task->kcp = kcp_client;
+		task->svr_addr = &param->serveraddr;
+		add_task_tail(task, &xkcp_task_list);
+		
+		while(1) {
+			char data[1024] = {0};
+			if (ikcp_recv(kcp_client, data, 1023) > 0) {
+				debug(LOG_DEBUG, "recv data is %s \n response is %s", data, response);
+				ikcp_send(kcp_client, response, strlen(response));
+			} else
+				break;
+		}
+	} 
 }
 
 static int set_xkcp_listener()
@@ -87,15 +137,7 @@ int server_main_loop()
 	
 	int xkcp_fd = set_xkcp_listener();
 	
-	struct xkcp_proxy_param  proxy_param;
-	memset(&proxy_param, 0, sizeof(proxy_param));
-	proxy_param.base 		= base;
-	proxy_param.udp_fd 		= xkcp_fd;
-	proxy_param.serveraddr.sin_family 	= AF_INET;
-	proxy_param.serveraddr.sin_port		= htons(xkcp_get_param()->local_port);
-	memcpy((char *)&proxy_param.serveraddr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
-	
-	xkcp_event = event_new(base, xkcp_fd, EV_READ|EV_PERSIST, xkcp_rcv_cb, &proxy_param);
+	xkcp_event = event_new(base, xkcp_fd, EV_READ|EV_PERSIST, xkcp_rcv_cb, NULL);
 	event_add(xkcp_event, NULL);
 	
 	event_assign(&timer_event, base, -1, EV_PERSIST, timer_event_cb, &timer_event);
