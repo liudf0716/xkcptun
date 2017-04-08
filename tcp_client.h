c5178679c90decbb83ac98e1eba3f97a64d5fba9
Dengfeng Liu <liudf0716@qq.com>
2017-04-08 14:49:19 +0800

    Update tcp_client.h

--- tcp_client.h
+++ tcp_client.h
@@ -1,4 +1,10 @@
 #ifndef	_TCP_CLIENT_
 #define _TCP_CLIENT_
 
+struct evconnlistener;
+struct sockaddr;
+
+void tcp_client_read_cb(struct evconnlistener *listener, evutil_socket_t fd,
+    					 struct sockaddr *a, int slen, void *param);
+
 #endif
