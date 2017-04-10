a556be1721ce8db5f340b1964a39b3dc4883db34
Dengfeng Liu <liudf0716@qq.com>
2017-04-10 15:02:04 +0800

    Update tcp_client.c

--- tcp_client.c
+++ tcp_client.c
@@ -21,9 +21,39 @@
 
 void tcp_client_event_cb(struct bufferevent *bev, short what, void *ctx)
 {
+	struct xkcp_task *task = ctx;
+
+	if (what & (BEV_EVENT_EOF|BEV_EVENT_ERROR)) {
+		if (task) {
+			del_task(task);
+			ikcp_release(task->kcp);
+			if (task->b_in != bev) {
+				bufferevent_free(task->b_in);
+				printf("impossible here\n");
+			}
+			free(task);
+		}
+		bufferevent_free(bev);
+	}
 }
 
 void tcp_client_read_cb(struct evconnlistener *listener, evutil_socket_t fd,
     struct sockaddr *a, int slen, void *param)
 {
+	struct xkcp_task *task = ctx;
+	ikcpcb *kcp = task->kcp;
+	struct evbuffer *src;
+	size_t	len;
+	
+	src = bufferevent_get_input(bev);
+	len = evbuffer_get_length(src);
+	
+	if (len > 0) {
+		char *data = malloc(len);
+		memset(data, 0, len);
+		evbuffer_copyout(src, data, len);
+		debug(LOG_DEBUG, "read data from server [%d]", len);
+		ikcp_send(kcp, data, len);
+		free(data);
+	}
 }
