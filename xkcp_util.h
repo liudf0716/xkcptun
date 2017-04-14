a1712d7e2263b649a7d9165cb874a7e42627a51d
Dengfeng Liu <liudf0716@qq.com>
2017-04-14 16:30:29 +0800

    Update xkcp_util.h

--- xkcp_util.h
+++ xkcp_util.h
@@ -42,6 +42,8 @@ void add_task_tail(struct xkcp_task *task, iqueue_head *head);
 
 void del_task(struct xkcp_task *task);
 
+void xkcp_set_config_param(ikcpcb *kcp);
+
 void xkcp_tcp_event_cb(struct bufferevent *bev, short what, struct xkcp_task *task);
 
 void xkcp_tcp_read_cb(struct bufferevent *bev, ikcpcb *kcp);
