f389bed96dddc888d1e69cc04bc5a4f68497cda4
Dengfeng Liu <liudf0716@qq.com>
2017-04-14 17:21:41 +0800

    Update tcp_proxy.c

--- tcp_proxy.c
+++ tcp_proxy.c
@@ -1,3 +1,24 @@
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -63,15 +84,13 @@ tcp_proxy_accept_cb(struct evconnlistener *listener, evutil_socket_t fd,
 
 	static int conv = 1;
 	ikcpcb *kcp_client 	= ikcp_create(conv, param);
-	kcp_client->output	= xkcp_output;
-	ikcp_wndsize(kcp_client, 128, 128);
-	ikcp_nodelay(kcp_client, 0, 10, 0, 1);
+	xkcp_set_config_param(kcp_client);
 	conv++;
 	
 	struct xkcp_task *task = malloc(sizeof(struct xkcp_task));
 	assert(task);
 	task->kcp = kcp_client;
-	task->b_in = b_in;
+	task->bev = b_in;
 	task->svr_addr = &p->serveraddr;
 	add_task_tail(task, &xkcp_task_list);
 
