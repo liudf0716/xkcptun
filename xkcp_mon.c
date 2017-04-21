e59299e13cc95c9524833fab24f3e2a1d713e046
Dengfeng Liu <liudf0716@qq.com>
2017-04-21 13:46:14 +0800

    Update xkcp_mon.c

--- xkcp_mon.c
+++ xkcp_mon.c
@@ -78,7 +78,7 @@ static void get_server_status(struct bufferevent *bev, void *ctx)
 	struct evbuffer *output = bufferevent_get_output(bev);
 	for(int i = 0; i < xkcp_hash->buckets; i++) {
 		jwHashEntry *entry = table->bucket[i];
-		evbuffer_add_printf(output, "client [%s]: \n", entry->key.strValue);
+		evbuffer_add_printf(output, "[%d] client [%s]: \n", i, entry->key.strValue);
 		dump_task_list(entry->value.ptrValue, bev);
 	}
 }
